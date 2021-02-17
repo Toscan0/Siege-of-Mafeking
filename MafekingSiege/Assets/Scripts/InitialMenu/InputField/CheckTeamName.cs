e209008bed89c915d011fda682b52a8bc7e96925
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-02-17 23:07:48 +0000

    enum and input field working

---
+++ MafekingSiege/Assets/Scripts/InitialMenu/InputField/CheckTeamName.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Dropdown))]
+public class CheckTeamName : MonoBehaviour
+{
+    // Dropdown text
+    [SerializeField]
+    private Text label;
+    [SerializeField]
+    private Color mandatoryFieldColor;
+    [SerializeField]
+    private Color defaultColor;
+
+    private enum teamNames {
+        None,
+        Bathing_Towell,
+        Baking_Powder,
+        M_Hala_Panzi,
+        Impisa,
+        Katankye,
+        Chief_Lone_Pine_On_Skyline
+    }
+    private string selectedTeamName = "";
+    private Dropdown dropdown;
+
+    private void Awake()
+    {
+        dropdown = GetComponent<Dropdown>();
+    }
+
+    private void Start()
+    {
+        PopulateList();
+        Dropdown_IndexChanged(0);
+    }
+
+    private void PopulateList()
+    {
+        string[] enumNames = Enum.GetNames(typeof(teamNames));
+        List<string> names = new List<string>(enumNames);
+        dropdown.AddOptions(names);
+    }
+
+    public void Dropdown_IndexChanged(int newIndex)
+    {
+        teamNames name = (teamNames)newIndex;
+        selectedTeamName = name.ToString();
+
+        if(newIndex == 0)
+        {
+            label.color = mandatoryFieldColor;
+        }
+        else
+        {
+            label.color = defaultColor;
+
+            //TODO : ALERT GAME MANAGER name defined
+        }
+    }
+}
---
+++ MafekingSiege/Assets/Scripts/InitialMenu/InputField/CheckUserName.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckUserName : MonoBehaviour
+{
+    private string selectedUserName = "";
+
+    public void Input_UserNameUpdated(string newName)
+    {
+        selectedUserName = newName;
+    }
+}
