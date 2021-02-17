b90aabaf5d5d2e4c13d419223e836d598e60683c
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-02-17 22:25:03 +0000

    load next scene working with fade out anim

---
+++ MafekingSiege/Assets/Scripts/InitialMenu/PlayButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayButton : MonoBehaviour
+{
+    [SerializeField]
+    private LoadSceneManager loadSceneManager;
+
+    public void Play()
+    {
+        loadSceneManager.LoadNextScene();
+    }
+}
---
+++ MafekingSiege/Assets/Scripts/Managers/LoadSceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject fadeOut;
+
+    internal void LoadNextScene()
+    {
+        fadeOut.SetActive(true);
+
+        StartCoroutine(LoadSceneByIndex(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    private IEnumerator LoadSceneByIndex(int indx)
+    {
+        yield return new WaitForSeconds(0.45f);
+        SceneManager.LoadScene(indx);
+    }
+}
