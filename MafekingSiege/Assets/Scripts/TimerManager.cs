26e922b2502fff556d5d2aea8a8c1a90ed6de66e
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-02-18 14:07:04 +0000

    timer added

---
+++ MafekingSiege/Assets/Scripts/TimerManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerManager : MonoBehaviour
+{
+    public static float CurrentTime { get; private set; } = 10 * 60; //in minutes
+
+    [SerializeField]
+    private Text timerText;
+
+    void Start()
+    {
+        SetTimerText();
+    }
+
+    void Update()
+    {
+        if (CurrentTime > 0)
+        {
+            CurrentTime -= Time.deltaTime;
+        }
+        else
+        {
+            CurrentTime = 0;
+        }
+        SetTimerText();
+    }
+
+    
+
+    private void SetTimerText()
+    {
+        if (timerText != null)
+        {
+            timerText.text = GetTimerAsString(CurrentTime);
+        }
+    }
+
+
+    ////
+    private string GetTimerAsString(float timer)
+    {
+        return GetMinutes(timer)
+        + ":" + GetSecondsToDisplay(timer) + ":"
+        + GetMilliseconds(timer);
+    }
+
+    private string GetMinutes(float seconds)
+    {
+        return ((int)seconds / 60).ToString();
+    }
+
+    private string GetSecondsToDisplay(float seconds)
+    {
+        return (seconds % 60).ToString("f0");
+    }
+
+    private string GetMilliseconds(float seconds)
+    {
+        return ((seconds * 1000) % 1000).ToString("f0");
+    }
+}
