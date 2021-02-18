c828035bfc925b844723644bc0b0e5c8aa768206
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-02-18 21:50:11 +0000

    connection to server implemented

---
+++ MafekingSiege/Assets/Scripts/LastMenu/SendToServer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SendToServer : MonoBehaviour
+{
+    [SerializeField]
+    private PostOnServer postOnServer;
+    [SerializeField]
+    private bool sendData = true;
+
+    private void Start()
+    {
+        if (sendData)
+        {
+            postOnServer.SendData(
+                GameManager.TeamName,
+                GameManager.UserName,
+                GameManager.UserName,
+                GameManager.UserName
+                );
+
+            postOnServer.SendData(
+                "BD",
+                "AI",
+                "555555",
+                "111");
+        }
+    }
+}
---
+++ MafekingSiege/Assets/Scripts/Server/PostOnServer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PostOnServer : MonoBehaviour
+{
+    private const string POST_HIGHSCORE_URL = "" +
+        "https://web.tecnico.ulisboa.pt/~ist181633/CatchTheFly/ServerSide/Post_HighScore.php";
+
+
+    public void SendData(string teamName, string userName, string time, string points)
+    {
+        string dataToSend = ParseDataToSend(teamName, userName, time, points);
+
+        StartCoroutine(dataToSend);
+    }
+
+    private string ParseDataToSend(string teamName, string userName, string time, string points)
+    {
+        return teamName + ";" + 
+            userName + ";" +
+            time + ";" +
+            points + ";";
+    }
+
+    private IEnumerator PostData(string data)
+    {
+        List<IMultipartFormSection> wwwForm = new List<IMultipartFormSection>();
+        wwwForm.Add(new MultipartFormDataSection("_content", data));
+
+        UnityWebRequest www = UnityWebRequest.Post(POST_HIGHSCORE_URL, wwwForm);
+
+        //w8 for answer
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.LogError("UnityWebRequest post error: " + www.error);
+        }
+
+        //TODO : Alert data sended
+    }
+}
