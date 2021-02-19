0237e97fc1eed262e5448b67bf86b3401b71d56e
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-02-19 21:00:54 +0000

    msg spawn

--- MafekingSiege/Assets/Scripts/MSG/MSGDeliver.cs
+++ MafekingSiege/Assets/Scripts/MSG/MSGDeliver.cs
@@ -1,21 +1,64 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class MSGDeliver : MonoBehaviour
 {
     [SerializeField]
     private GameObject Parchment;
 
+    private bool hasMSG;
+    private int msgProbability = 50;
+    private float minReloadTime = 1f;
+    private float maxReloadTime = 4f;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnMSG());
+    }
+
+    private IEnumerator SpawnMSG()
+    {
+        while (true)
+        {
+            if (UnityEngine.Random.Range(0, 100) <= msgProbability)
+            {
+                NewMSG();
+            }
+            else
+            {
+                NoMSG();
+            }
+
+            yield return new WaitForSeconds(UnityEngine.Random.Range(minReloadTime, maxReloadTime));
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        IMessageable messageable = collision.gameObject.GetComponent<IMessageable>();
-        if (messageable != null)
+        if (hasMSG)
         {
-            bool msgReceived = messageable.TakeMSG();
-
-            if (msgReceived)
+            IMessageable messageable = collision.gameObject.GetComponent<IMessageable>();
+            if (messageable != null)
             {
-                Parchment.SetActive(false);
+                bool msgReceived = messageable.TakeMSG();
+
+                if (msgReceived)
+                {
+                    NoMSG();
+                }
             }
         }
+    }
+
+    private void NoMSG()
+    {
+        Parchment.SetActive(false);
+        hasMSG = false;
+    }
+
+    private void NewMSG()
+    {
+        Parchment.SetActive(true);
+        hasMSG = true;
     }
 }
