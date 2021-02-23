7949c59fa11f91735d0b9948bdb6f685dd812539
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-02-23 15:47:21 +0000

    delaer spawn done

---
+++ MafekingSiege/Assets/Scripts/MSG/DeliverMSGSpawner.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliverMSGSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject spawn1;
+    [SerializeField]
+    private GameObject spawn2;
+
+    public bool DealerEnabled { private get; set; } = false;
+    private float minReloadTime = 0.5f;
+    private float maxReloadTime = 3f;
+
+    private void OnDrawGizmos()
+    {
+        if (spawn1 != null)
+        {
+            Gizmos.color = new Color(0, 1, 0, 0.5f);
+            Gizmos.DrawCube(spawn1.transform.position, new Vector3(1.5f, 1.5f, 1));
+        }
+        if (spawn2 != null)
+        {
+            Gizmos.color = new Color(0, 1, 0, 0.5f);
+            Gizmos.DrawCube(spawn2.transform.position, new Vector3(1.5f, 1.5f, 1));
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SpawnMSG());
+    }
+
+    private IEnumerator SpawnMSG()
+    {
+        while (true)
+        {
+            if (!DealerEnabled)
+            {
+                if (UnityEngine.Random.Range(0, 100) <= 50)
+                { // spawn 1
+                    spawn1.SetActive(true);
+                    spawn2.SetActive(false);
+                    DealerEnabled = true;
+                }
+                else
+                { // spawn 2
+                    spawn1.SetActive(false);
+                    spawn2.SetActive(true);
+                    DealerEnabled = true;
+                }
+            }
+
+            yield return new WaitForSeconds(UnityEngine.Random.Range(minReloadTime, maxReloadTime));
+        }
+    }
+}
--- MafekingSiege/Assets/Scripts/MSG/MSGDeliver.cs
+++ MafekingSiege/Assets/Scripts/MSG/MSGDeliver.cs
@@ -1,83 +1,37 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
-[RequireComponent(typeof(SpriteRenderer))]
-[RequireComponent(typeof(Collider2D))]
 public class MSGDeliver : MonoBehaviour
 {
-    private bool hasMSG = false;
+    [SerializeField]
+    private DeliverMSGSpawner deliverMSGSpawner;
     private int msgProbability = 50;
-    private float minReloadTime = 0.5f;
-    private float maxReloadTime = 3f;
 
-    private SpriteRenderer spriteRenderer;
-    private Collider2D col2D;
     private Animator animator;
 
     private void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        col2D = GetComponent<Collider2D>();
         animator = GetComponent<Animator>();
     }
 
-    private void Start()
+    private void OnEnable()
     {
-        StartCoroutine(SpawnMSG());
-    }
-
-    private IEnumerator SpawnMSG()
-    {
-        while (true)
-        {
-            if (!hasMSG)
-            {
-                if (UnityEngine.Random.Range(0, 100) <= msgProbability)
-                {
-                    NewMSG();
-                }
-                else
-                {
-                    NoMSG();
-                }
-            }
-
-            yield return new WaitForSeconds(UnityEngine.Random.Range(minReloadTime, maxReloadTime));
-        }
+        SelectNPC();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (hasMSG)
+        IMessageable messageable = collision.gameObject.GetComponent<IMessageable>();
+        if (messageable != null)
         {
-            IMessageable messageable = collision.gameObject.GetComponent<IMessageable>();
-            if (messageable != null)
-            {
-                bool msgReceived = messageable.TakeMSG();
+            bool msgReceived = messageable.TakeMSG();
 
-                if (msgReceived)
-                {
-                    NoMSG();
-                }
+            if (msgReceived)
+            {
+                deliverMSGSpawner.DealerEnabled = false;
+                gameObject.SetActive(false);
             }
         }
-    }
-
-    private void NoMSG()
-    {
-        spriteRenderer.enabled = false;
-        col2D.enabled = false;
-        hasMSG = false;
-    }
-
-    private void NewMSG()
-    {
-        SelectNPC();
-
-        spriteRenderer.enabled = true;
-        col2D.enabled = true;
-        hasMSG = true;
     }
 
     private void SelectNPC()
